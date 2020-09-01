f083d28930e914c26b24bbfe8fcf4adf721727ed
spoonkillel2 <spoonkillel2@gmail.com>
2020-09-01 16:17:13 +0700

    01/09/2020
    Excercise 4

---
+++ assignments/Exercise4_Wichai_W.py
@@ -0,0 +1,9 @@
+foundation_English = 69.4
+general_Business = 87.7
+introduction_Computer_System = 45.6
+computer_Programming = 46
+
+print("Foundation English :", foundation_English, "Point")
+print("General Business :", general_Business, "Point")
+print("Introduction to Computer System :", introduction_Computer_System, "Point")
+print("Computer_Programing :", computer_Programming, "Point")
