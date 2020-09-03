a2dcd0a9152e46937b29f958413f32510b81f16c
spoonkillel2 <spoonkillel2@gmail.com>
2020-09-03 16:56:43 +0700

    Excercise5

---
+++ assignments/Excercise5_1_Wichai_W.py
@@ -0,0 +1,13 @@
+num1 = int(input("Num1 : "))
+num2 = int(input("Num2 : "))
+
+result1 = num1 + num2
+result2 = num1 - num2
+result3 = num1 * num2
+result4 = num1 / num2
+
+print ("Your Result :")
+print (num1,"+",num2,"=", result1)
+print (num1,"-",num2,"=", result2)
+print (num1,"*",num2,"=", result3)
+print (num1,"/",num2,"=", result4)
---
+++ assignments/Excercise5_2_Wichai_W.py
@@ -0,0 +1,6 @@
+s = int(input("Length (Km) : "))
+t =  int(input("Time (Hr) : "))
+v = s/t
+
+print ("Your speed :", v,"km/h")
+
